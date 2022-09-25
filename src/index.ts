35cc3c646ba095691bce8115bb0cadd86585dd98
WBomfim <willianbomfim@hotmail.com>
2022-09-25 11:10:49 -0300

    chore: Criado o arquivo index. - No arquivo foram criadas algumas instâncias dos personagens e montros que temos no jogo, também foi criado algumas batalhas e uma função que executa as batalhas instanciadas.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+import Battle, { PVE, PVP } from './Battle';
 import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
-const character = new Character('John');
+const player1 = new Character('Bob');
+const player2 = new Character('Tom');
+const player3 = new Character('Sam');
 
-console.log(character);
+for (let i = 1; i <= 3; i += 1) {
+  player1.levelUp();
+}
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]): void => {
+  battles.forEach((battle) => battle.fight());
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
